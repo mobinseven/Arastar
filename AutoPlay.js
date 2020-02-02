8f984b55ac119adc80b0e855956408f7974fe619
mobinseven <mobin.seven@live.com>
2020-02-02 15:45:25 +0330

    D3Glow+DummyScroll to hid address bar in safari

--- AutoPlay.js
+++ AutoPlay.js
@@ -22,6 +22,12 @@ window.CreateSvgElementInContainer = function(url, ContainerId) {
                 $(path).css("stroke", "#D4A440");
                 //     $(path).css("animation", "glow 5s infinite 2s ease");
             });
+        $(svgElem).attr(
+            "id",
+            $(svgElem)
+                .children("title")
+                .text()
+        );
         $(svgElem)
             .children("title")
             .remove();
@@ -32,12 +38,24 @@ $(document).ready(function() {
     // CreateSvgElementInContainer("assets/images/selfie.svg", "selfie");
     // CreateSvgElementInContainer("assets/images/jenner.svg", "jenner");
     // CreateSvgElementInContainer("assets/images/lima.svg", "lima");
-    CreateSvgElementInContainer("assets/images/couple.svg", "Mk2");
+    CreateSvgElementInContainer("assets/images/kiss.svg", "love");
     CreateSvgElementInContainer("assets/images/brusher.svg", "brusher");
-    CreateSvgElementInContainer("assets/images/dancing2.svg", "dancing");
-    CreateSvgElementInContainer("assets/images/ashley.svg", "ashley");
-    CreateSvgElementInContainer("assets/images/lisa.svg", "lisa");
+    CreateSvgElementInContainer("assets/images/dancing.svg", "dancing");
+    CreateSvgElementInContainer("assets/images/ashley.svg", "beauty");
+    CreateSvgElementInContainer("assets/images/lisa.svg", "hands");
     CreateSvgElementInContainer("assets/images/ArastarOutline.svg", "logo");
+    CreateSvgElementInContainer(
+        "assets/images/persianstyle-1.svg",
+        "persian-style"
+    );
+    CreateSvgElementInContainer(
+        "assets/images/persianstyle-smile.svg",
+        "persian-smile"
+    );
+    CreateSvgElementInContainer(
+        "assets/images/persianstyle-sky.svg",
+        "persian-sky"
+    );
 
     $("#carousel").on("slid.bs.carousel", function() {
         var svgElem = $("#carousel")
@@ -69,19 +87,54 @@ $(document).ready(function() {
         //     .find("svg")
         //     .find("path")[0];
 
-        $(svgElem).queue("paths", function() {
-            if ($(svgElem).find("defs>filter").length > 0) {
-                $(svgElem).find(
-                    "defs>filter#inset>:nth-child(6)"
-                )[0].style.WebkitAnimation = "glow 10s infinite 2s ease";
-                $(svgElem).find(
-                    "defs>filter#inset>:nth-child(6)"
-                )[0].style.animation = "glow 10s infinite 2s ease";
-                $(svgElem)
-                    .find("g")
-                    .attr("filter", "url(#inset)");
+        if ($(svgElem).find("g.glow").length > 0) {
+            var filterId = "glow-" + svgElem.id;
+            if ($(svgElem).find("#" + filterId).length == 0) {
+                var svg = d3.select(svgElem);
+                var viewBox = svgElem.getAttribute("viewBox").split(" ");
+                viewBox[0] = parseInt(viewBox[0]) - 20;
+                viewBox[1] = parseInt(viewBox[1]) - 20;
+                viewBox[2] = 20 + parseInt(viewBox[2]);
+                viewBox[3] = 20 + parseInt(viewBox[3]);
+                svgElem.setAttribute("viewBox", viewBox.join(" "));
+                var filter = svg
+                    .append("filter")
+                    .attr("id", filterId)
+                    .attr("x", "-.3")
+                    .attr("y", "-.3")
+                    .attr("height", "1.5")
+                    .attr("width", "1.5");
+                filter
+                    .append("feMorphology")
+                    .attr("in", "SourceGraphic")
+                    .attr("operator", "dilate")
+                    .attr("radius", "9");
+                filter
+                    .append("feGaussianBlur")
+                    .attr("stdDeviation", "10")
+                    .attr("result", "coloredBlur");
+                var feTransfer = filter.append("feComponentTransfer");
+
+                var alpha = feTransfer
+                    .append("feFuncA")
+                    .attr("type", "linear")
+                    .attr("slope", 0);
+                var feMerge = filter.append("feMerge");
+                feMerge.append("feMergeNode");
+                feMerge.append("feMergeNode").attr("in", "SourceGraphic");
+                alpha
+                    .attr("slope", 0)
+                    .transition()
+                    .duration(3000)
+                    .ease(d3.easeLinear)
+                    .attr("slope", .5);
             }
-        });
+
+            $(svgElem).queue("paths", function() {
+                $(svgElem).find("g.glow").attr("filter", "url(#" + filterId + ")");
+                // var alpha = d3.select($(svgElem).find("feFuncA")[0]);
+            });
+        }
     });
     $("#carousel").on("slide.bs.carousel", function() {
         var lastSvg = $("#carousel")
@@ -90,15 +143,9 @@ $(document).ready(function() {
         $(lastSvg)
             .clearQueue("paths")
             .finish();
-        if ($(lastSvg).find("defs>filter").length > 0) {
-            $(lastSvg).find(
-                "defs>filter#inset>:nth-child(6)"
-            )[0].style.WebkitAnimation = "";
-            $(lastSvg).find(
-                "defs>filter#inset>:nth-child(6)"
-            )[0].style.animation = "";
+        if ($(lastSvg).find("g.glow").length > 0) {
             $(lastSvg)
-                .find("g")
+                .find("g.glow")
                 .attr("filter", "");
         }
         $(lastSvg)
---
+++ sw.js
@@ -0,0 +1,39 @@
+//A version number is useful when updating the worker logic, allowing you to remove outdated cache entries
+var version = "v1::2";
+self.addEventListener("install", function(event) {
+    console.log("WORKER: install event in progress.");
+    event.waitUntil(
+        /* The caches built-in is a promise-based API that helps you cache responses,
+             as well as finding and deleting them.
+          */
+        caches
+            /* You can open a cache by name, and this method returns a promise. We use
+               a versioned cache name here so that we can remove old cache entries in
+               one fell swoop later, when phasing out an older service worker.
+            */
+            .open(version + "fundamentals")
+            .then(function(cache) {
+                cache.addAll([
+                    // levels 11-20
+                ]);
+                /* After the cache is opened, we can fill it with the offline fundamentals.
+                 The method below will add all resources we've indicated to the cache,
+                 after making HTTP requests for each of them.
+              */
+                return cache.addAll([
+                    // core assets & levels 1-10
+
+                    "/",
+                    "/assets/jquery-3.4.1.min.js",
+                    "/assets/bootstrap.css",
+                    "/assets/BNazanin.woff",
+                    "/assets/Socicon.woff2",
+                    "assets/bootstrap.min.js",
+                    "assets/d3.v5.min.js"
+                ]);
+            })
+            .then(function() {
+                console.log("WORKER: install completed");
+            })
+    );
+});
